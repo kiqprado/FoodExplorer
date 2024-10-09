53ede9dacbc1621a979d48c7b4023efa430058d7
Kaique Prado <kiqprado@outlook.com>
2024-10-09 12:46:12 -0300

    feat: Creatte Section

---
+++ src/components/Section/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  h2 {
+    font-size: 1.8rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+
+    margin-bottom: 2.4rem;
+  }
+`
